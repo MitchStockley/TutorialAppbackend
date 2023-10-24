2cbf8e1a6ba5acd4a4d36d5adb1d41a0831bede9
Mitchell <mitchell.stockley@gmail.com>
2023-10-24 15:29:45 +0200

    update

--- routes/tutorial.routes.js
+++ routes/tutorial.routes.js
@@ -4,25 +4,25 @@ module.exports = app => {
     var router = require('express').Router();
 
     //create a new tutorial
-    router.post("/", tutorials.create);
+    router.post("/https://tutorialsbackend.onrender.com/", tutorials.create);
 
     //retrieve all tutorials
-    router.get("/", tutorials.findAll);
+    router.get("/https://tutorialsbackend.onrender.com/", tutorials.findAll);
 
     //retrieve all published tutorials
-    router.get("/published", tutorials.findAllPublished);
+    router.get("/https://tutorialsbackend.onrender.com/published", tutorials.findAllPublished);
 
     //retrieve a single tutorials with id
-    router.get("/:id", tutorials.findOne);
+    router.get("/https://tutorialsbackend.onrender.com/:id", tutorials.findOne);
 
     //update a tutorial with id
-    router.put("/:id", tutorials.update);
+    router.put("/https://tutorialsbackend.onrender.com/:id", tutorials.update);
 
     //delete a tutorial with an id
-    router.delete("/:id", tutorials.delete)
+    router.delete("/https://tutorialsbackend.onrender.com/:id", tutorials.delete)
 
     //delete all tutorials 
-    router.delete("/", tutorials.deleteAll);
+    router.delete("/https://tutorialsbackend.onrender.com/", tutorials.deleteAll);
 
-    app.use("/api/tutorials", router);
+    app.use("/https://tutorialsbackend.onrender.com/", router);
 }
